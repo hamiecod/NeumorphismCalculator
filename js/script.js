f2081dee80d498b5a9c9d2b36e7934c07b68fd2a
hamiecod <hargunbeersingh@gmail.com>
2021-02-08 18:35:27 +0530

    some minor changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 var buttons = document.getElementsByClassName('btn');
 
 // for loop to set different CSS properties
@@ -8,20 +7,35 @@ for (let i = 0; i < buttons.length; i++){
     trigger.style.transitionDuration='0.15s';
     trigger.style.transitionProperty='all';
 
-    // setting the css properties
-    buttons[i].addEventListener('click', function hoverButton(){
+    // adding css styling
+    buttons[i].addEventListener('click', ()=>{
         if(trigger.style.backgroundColor != 'inset -8px -8px 16px rgba(255, 255, 255, 0.58), 8px 8px 16px rgba(0, 0, 0, 0.25)'){
             trigger.style.boxShadow='inset -8px -8px 16px rgba(255, 255, 255, 0.58), inset 8px 8px 16px rgba(0, 0, 0, 0.25)';
         }
         setTimeout(() => {
             trigger.style.boxShadow = '-8px -8px 16px rgba(255, 255, 255, 0.58), 8px 8px 16px rgba(0, 0, 0, 0.25)';
         }, 150);
-
-        let value = buttons[i].innerText;
-        console.log(value);
     })
 
-    // building the logic of the program
-    // buttons[i].addEventListener('click')
+    buttons[i].addEventListener('click', ()=>{
+        let idStorer =buttons[i].id;
+        let trigger = document.getElementById(idStorer);
+        let answerBox = document.getElementById('answerBox');
+        let value = trigger.innerText;
+        answerBox.innerText += value;
+
+        if(document.getElementById(buttons[i].id).innerText = '×'){
+            answerBox.innerText += '*';
+        }
+        else if(value == "AC"){
+            answerKey.innerText = '';
+        }
+        else if(value == '='){
+            value = "";
+            answerBox.innerText = Math.eval(answerBox.innerText)
+        }
+    })
 }
 
+// let answerBox = document.getElementById('answerBox');
+// if (answerBox.innerText)
